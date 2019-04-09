f2ed09e0b7f5e9d03559964e3a52421645529539
unknown <huangyy@swiet.com.cn>
2019-04-09 10:12:39 +0800

    class HelloWorld

--- src/component/hello.js
+++ src/component/hello.js
@@ -1,8 +1,16 @@
-import React from 'react';
-const HelloWorld = (props)=>{
-    console.log(props);
-    return(
-        <div>{props.name} say:hello world</div>
-    )
+import React,{Component} from 'react';
+class HelloWorld extends Component{
+    clickHander=()=>{
+        console.log(this.props.name);
+        console.log(this.props);
+    }
+
+    render(){
+        return(
+            <div onClick={this.clickHander}>
+            {this.props.name} say:hello world
+            </div>
+        )
+    }
 }
 export default HelloWorld
